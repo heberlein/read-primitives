5669fb97bfc6582c49e64cb4a64dd1f322cde481
heberlein <heberlein@proton.me>
2023-02-06 17:22:11 +0100

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,37 @@
+/*!
+read-primitives provides traits to read primitive types from any type that implements [std::io::Read](https://doc.rust-lang.org/std/io/trait.Read.html)
+
+ # Examples
+
+```
+    use read_primitives::ReadF64;
+    let bytes: [u8; 8] = [24, 45, 68, 84, 251, 33, 9, 64];
+    let float = bytes.as_slice().read_le_f64().unwrap();
+    assert_eq!(std::f64::consts::PI, float)
+```
+*/
+
 use std::io::{self, Read};
 
 macro_rules! impl_traits {
     ($($type:ty),+) => {
         $(
             ::paste::paste!{
+                #[doc = "Trait to read "$type "."]
                 pub trait [<Read $type:camel>]: Read {
+                    #[doc = "Read " $type "in native byte order"]
                     fn [<read_ne_  $type>](&mut self) -> io::Result<$type> {
                         let mut bytes = [0u8; std::mem::size_of::<$type>()];
                         self.read_exact(&mut bytes)?;
                         Ok($type::from_ne_bytes(bytes))
                     }
+                    #[doc = "Read " $type "in little endian byte order"]
                     fn [<read_le_  $type>](&mut self) -> io::Result<$type> {
                         let mut bytes = [0u8; std::mem::size_of::<$type>()];
                         self.read_exact(&mut bytes)?;
                         Ok($type::from_le_bytes(bytes))
                     }
+                    #[doc = "Read " $type "in big endian byte order"]
                     fn [<read_be_  $type>](&mut self) -> io::Result<$type> {
                         let mut bytes = [0u8; std::mem::size_of::<$type>()];
                         self.read_exact(&mut bytes)?;
@@ -29,7 +46,9 @@ macro_rules! impl_traits {
 
 impl_traits!(u16, i16, u32, i32, u64, i64, u128, i128, usize, isize, f32, f64);
 
+/// Trait to read u8
 pub trait ReadU8: Read {
+    /// Read a u8
     fn read_u8(&mut self) -> io::Result<u8> {
         let mut bytes = [0u8; 1];
         self.read_exact(&mut bytes)?;
@@ -38,7 +57,10 @@ pub trait ReadU8: Read {
 }
 impl<R> ReadU8 for R where R: Read {}
 
+/// Trait to read char
 pub trait ReadChar: Read {
+    /// Read a char
+    ///  It is a assumed that the char is represented in native yte order
     fn read_char(&mut self) -> io::Result<Option<char>> {
         let mut bytes = [0u8; 4];
         self.read_exact(&mut bytes)?;
@@ -46,7 +68,10 @@ pub trait ReadChar: Read {
     }
 }
 impl<R> ReadChar for R where R: Read {}
+
+/// Trait to read char
 pub trait ReadBool: Read {
+    /// Read a bool
     fn read_bool(&mut self) -> io::Result<bool> {
         let mut bytes = [0u8; 1];
         self.read_exact(&mut bytes)?;
